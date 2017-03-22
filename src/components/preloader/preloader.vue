342942e4d52f2df80e16bd8219027fbe10f8cf75
zhangcong <zhangcong11@meituan.com>
2017-03-22 09:39:00 +0800

    add preloader component

---
+++ src/components/preloader/preloader.vue
@@ -0,0 +1,30 @@
+<template>
+    <section id="preloader">
+        <div class="loader fa fa-spin"></div>
+    </section>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/css/font-awesome.css"
+
+    #preloader
+      position: fixed
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      background: #fff
+      z-index: 1000
+      .loader
+        position: absolute
+        margin: -25px 0 0 -25px
+        top: 50%
+        left: 50%
+        width: 50px
+        height: 50px
+        background: url(loader.png)
+        background-size: 50px 50px
+</style>
--- src/module/index/App.vue
+++ src/module/index/App.vue
@@ -1,26 +1,16 @@
 <template>
 	<div>
-		<vhead></vhead>
-		<vindex></vindex>
-		<vfoot></vfoot>
+		<preloader></preloader>
 	</div>
 </template>
 
 <script>
-
-import vhead from '../../components/common/Head'
-import vfoot from '../../components/common/Foot'
-import vindex from '../../components/index/Index'
-export default {
-	components: {
-      // 可以以key-value的形式注册组件, 此时挂载点的名字就是key
-      // 否则挂载点和组件名字一致, 即vhead
-      vhead,
-      vfoot,
-      vindex
-    }
-}
-
+	import preloader from '../../components/preloader/preloader'
+	export default {
+		components: {
+			preloader
+		}
+	}
 </script>
 
 <style>
