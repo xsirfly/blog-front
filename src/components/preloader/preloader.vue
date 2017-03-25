0e15f21431709e2926c001fc8011fd53fde18853
zhangcong <zhangcong11@meituan.com>
2017-03-25 16:22:22 +0800

    use scss and complete preloader component

--- src/components/preloader/preloader.vue
+++ src/components/preloader/preloader.vue
@@ -1,30 +1,44 @@
 <template>
-    <section id="preloader">
-        <div class="loader fa fa-spin"></div>
-    </section>
+    <transition name="fade">
+        <section v-if="show" id="preloader">
+            <div class="loader fa fa-spin"></div>
+        </section>
+    </transition>
 </template>
 
 <script type="text/ecmascript-6">
+    export default {
+        props: ['show']
+    }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-    @import "../../common/css/font-awesome.css"
+<style lang="scss" rel="stylesheet/scss">
+    @import "../../common/css/font-awesome.css";
+    .fade-enter-active, .fade-leave-active{
+        transition: opacity .5s
+    }
 
-    #preloader
-      position: fixed
-      top: 0
-      left: 0
-      width: 100%
-      height: 100%
-      background: #fff
-      z-index: 1000
-      .loader
-        position: absolute
-        margin: -25px 0 0 -25px
-        top: 50%
-        left: 50%
-        width: 50px
-        height: 50px
-        background: url(loader.png)
-        background-size: 50px 50px
+    .fade-enter, .fade-leave-active{
+        opacity: 0
+    }
+
+    #preloader{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #fff;
+        z-index: 1000;
+        .loader{
+            position: absolute;
+            margin: -25px 0 0 -25px;
+            top: 50%;
+            left: 50%;
+            width: 50px;
+            height: 50px;
+            background: url(loader.png);
+            background-size: 50px 50px;
+        }
+    }
 </style>
--- src/module/index/App.vue
+++ src/module/index/App.vue
@@ -1,12 +1,17 @@
 <template>
 	<div>
-		<preloader></preloader>
+		<preloader v-bind:show="preloaderShow"></preloader>
 	</div>
 </template>
 
 <script>
 	import preloader from '../../components/preloader/preloader'
 	export default {
+		data() {
+			return {
+				preloaderShow: true
+			}
+		},
 		components: {
 			preloader
 		}
